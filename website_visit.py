388ff0f1e7eb73c58a1823dd7b150bc25ace85c7
vatsala <vatsala.swaroop@gmail.com>
2016-03-26 02:17:13 +0530

    Convert to test case

--- website_visit.py
+++ website_visit.py
@@ -1,17 +1,32 @@
 import time
+import unittest
 from selenium import webdriver
 
-driver = webdriver.Firefox()
+class YouTube(unittest.TestCase):
 
-#Proxy configurations for my system
-#profile = webdriver.FirefoxProfile()
-#profile.set_preference("network.proxy.type", 1)
-#profile.set_preference("network.proxy.http", "172.31.16.10")
-#profile.set_preference("network.proxy.http_port", "8080")
-#profile.update_preferences()
-#driver = webdriver.Firefox(firefox_profile=profile)"""
+    def setUp(self):
 
+    	#Proxy configurations for my system
+    	#profile = webdriver.FirefoxProfile()
+    	#profile.set_preference("network.proxy.type", 1)
+    	#profile.set_preference("network.proxy.http", "172.31.16.10")
+    	#profile.set_preference("network.proxy.http_port", "8080")
+    	#profile.update_preferences()
+    	#driver = webdriver.Firefox(firefox_profile=profile)"""
+        self.homepage = 'https://www.youtube.com/'
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(5)
+        self.driver.maximize_window()
 
-driver.get('https://www.youtube.com/')
-time.sleep(15)
-driver.quit()
+    def test_youtube(self):
+        driver = self.driver
+        driver.get('https://www.youtube.com/')
+        #match tite and url
+        self.assertIn("YouTube", driver.title)
+        self.assertEqual(self.driver.current_url,self.homepage)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
